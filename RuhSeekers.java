43d98f039e9bed6e3944fa398ed84ba051a0a420
Supun Tharaka <you@example.com>
2017-10-28 10:58:19 +0530

    First Round

--- RuhSeekers.java
+++ RuhSeekers.java
@@ -1,3 +1,55 @@
-class RuhSeekers{
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ruhseekers;
+
+import robocode.AlphaBot;
+import robocode.HitByBulletEvent;
+import robocode.HitWallEvent;
+import robocode.ScannedRobotEvent;
+
+/**
+ *
+ * @author supun
+ */
+public class RuhSeekers extends AlphaBot{
+
+   
+    public static void main(String[] args) {
+        
+    }
+   
+	public void run() {
+		
+		while(true) {
+			// Replace the next 4 lines with any behavior you would like
+			ahead(100);
+			turnGunRight(360);
+			back(100);
+			turnGunRight(360);
+		}
+	}
+
 	
+	public void onScannedRobot(ScannedRobotEvent e) {
+		// Replace the next line with any behavior you would like
+		fire(1);
+	}
+
+	
+	public void onHitByBullet(HitByBulletEvent e) {
+		// Replace the next line with any behavior you would like
+		back(10);
+	}
+	
+	
+	public void onHitWall(HitWallEvent e) {
+		// Replace the next line with any behavior you would like
+		back(20);
+	}
+
+    
 }
+
