d4fcd71a35fd56926cbac06bf5e08e5efd6a9863
Supun Tharaka <you@example.com>
2017-10-28 11:01:52 +0530

    changed

--- RuhSeekers.java
+++ RuhSeekers.java
@@ -1,55 +1,80 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package ruhseekers;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
 import robocode.AlphaBot;
+import robocode.BattleEndedEvent;
+import robocode.Bullet;
+import robocode.BulletHitBulletEvent;
+import robocode.BulletHitEvent;
+import robocode.BulletMissedEvent;
+import robocode.DeathEvent;
 import robocode.HitByBulletEvent;
+import robocode.HitRobotEvent;
 import robocode.HitWallEvent;
+import robocode.RobotDeathEvent;
+import robocode.RoundEndedEvent;
 import robocode.ScannedRobotEvent;
+import robocode.StatusEvent;
+import robocode.WinEvent;
 
 /**
  *
- * @author supun
+ * @author Pradeep Madushan
  */
-public class RuhSeekers extends AlphaBot{
+public class RuhSeeker extends AlphaBot {
 
-   
-    public static void main(String[] args) {
-        
-    }
-   
+boolean peek; 
+	double moveAmount; 
 	public void run() {
+		// Set colors
+		setBodyColor(Color.black);
+		setGunColor(Color.black);
+		setRadarColor(Color.orange);
+		setBulletColor(Color.cyan);
+		setScanColor(Color.cyan);
+
+		// Initialize moveAmount to the maximum possible for this battlefield.
+		moveAmount = Math.max(getBattleFieldWidth(), getBattleFieldHeight());
+		// Initialize peek to false
+		peek = false;
+
 		
-		while(true) {
-			// Replace the next 4 lines with any behavior you would like
-			ahead(100);
-			turnGunRight(360);
-			back(100);
-			turnGunRight(360);
+		turnLeft(getHeading() % 90);
+		ahead(moveAmount);
+		peek = true;
+		turnGunRight(90);
+		turnRight(90);
+
+		while (true) {
+		
+			peek = true;
+	
+			ahead(moveAmount);
+			
+			peek = false;
+			
+			turnRight(90);
 		}
 	}
 
 	
+	public void onHitRobot(HitRobotEvent e) {
+		
+		if (e.getBearing() > -90 && e.getBearing() < 90) {
+			back(100);
+		} 
+		else {
+			ahead(100);
+		}
+	}
+
 	public void onScannedRobot(ScannedRobotEvent e) {
-		// Replace the next line with any behavior you would like
-		fire(1);
+		fire(2);
+		if (peek) {
+			scan();
+		}
 	}
-
-	
-	public void onHitByBullet(HitByBulletEvent e) {
-		// Replace the next line with any behavior you would like
-		back(10);
-	}
-	
-	
-	public void onHitWall(HitWallEvent e) {
-		// Replace the next line with any behavior you would like
-		back(20);
-	}
-
-    
 }
-
